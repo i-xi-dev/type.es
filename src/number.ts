3617592f334865442440b524292e561b4022ef45
i-xi-dev <naoyuki.isono@gmail.com>
2024-10-18 23:31:22 +0900

    1.1.0

--- src/number.ts
+++ src/number.ts
@@ -2,6 +2,22 @@ export function isNumber(test: unknown): test is number {
   return (typeof test === "number");
 }
 
+export function isPositiveNumber(test: unknown): test is number {
+  return isNumber(test) && (test > 0);
+}
+
+export function isNonNegativeNumber(test: unknown): test is number {
+  return isNumber(test) && (test >= 0);
+}
+
+export function isNonPositiveNumber(test: unknown): test is number {
+  return isNumber(test) && (test <= 0);
+}
+
+export function isNegativeNumber(test: unknown): test is number {
+  return isNumber(test) && (test < 0);
+}
+
 export function isSafeInteger(test: unknown): test is number {
   return Number.isSafeInteger(test);
 }
--- tests/number.test.ts
+++ tests/number.test.ts
@@ -2,7 +2,11 @@ import { assertStrictEquals, fail, unreachable } from "./deps.ts";
 import {
   assertNumber,
   assertSafeInteger,
+  isNegativeNumber,
+  isNonNegativeNumber,
+  isNonPositiveNumber,
   isNumber,
+  isPositiveNumber,
   isSafeInteger,
 } from "../mod.ts";
 
@@ -126,6 +130,126 @@ Deno.test("isNumber()", () => {
   assertStrictEquals(isNumber(false), false);
   assertStrictEquals(isNumber(""), false);
   assertStrictEquals(isNumber("0"), false);
+});
+
+Deno.test("isPositiveNumber()", () => {
+  assertStrictEquals(isPositiveNumber(0), false);
+  assertStrictEquals(isPositiveNumber(-0), false);
+  assertStrictEquals(isPositiveNumber(1), true);
+  assertStrictEquals(isPositiveNumber(-1), false);
+
+  assertStrictEquals(isPositiveNumber(-10.1), false);
+  assertStrictEquals(isPositiveNumber(-9.9), false);
+  assertStrictEquals(isPositiveNumber(9.9), true);
+  assertStrictEquals(isPositiveNumber(10.1), true);
+
+  assertStrictEquals(isPositiveNumber(0n), false);
+  assertStrictEquals(isPositiveNumber(-0n), false);
+  assertStrictEquals(isPositiveNumber(1n), false);
+  assertStrictEquals(isPositiveNumber(-1n), false);
+
+  assertStrictEquals(isPositiveNumber(Number.NaN), false);
+  assertStrictEquals(isPositiveNumber(Number.POSITIVE_INFINITY), true);
+  assertStrictEquals(isPositiveNumber(Number.MAX_SAFE_INTEGER), true);
+  assertStrictEquals(isPositiveNumber(Number.MIN_SAFE_INTEGER), false);
+  assertStrictEquals(isPositiveNumber(Number.NEGATIVE_INFINITY), false);
+
+  assertStrictEquals(isPositiveNumber(undefined), false);
+  assertStrictEquals(isPositiveNumber(null), false);
+  assertStrictEquals(isPositiveNumber(true), false);
+  assertStrictEquals(isPositiveNumber(false), false);
+  assertStrictEquals(isPositiveNumber(""), false);
+  assertStrictEquals(isPositiveNumber("0"), false);
+});
+
+Deno.test("isNonNegativeNumber()", () => {
+  assertStrictEquals(isNonNegativeNumber(0), true);
+  assertStrictEquals(isNonNegativeNumber(-0), true);
+  assertStrictEquals(isNonNegativeNumber(1), true);
+  assertStrictEquals(isNonNegativeNumber(-1), false);
+
+  assertStrictEquals(isNonNegativeNumber(-10.1), false);
+  assertStrictEquals(isNonNegativeNumber(-9.9), false);
+  assertStrictEquals(isNonNegativeNumber(9.9), true);
+  assertStrictEquals(isNonNegativeNumber(10.1), true);
+
+  assertStrictEquals(isNonNegativeNumber(0n), false);
+  assertStrictEquals(isNonNegativeNumber(-0n), false);
+  assertStrictEquals(isNonNegativeNumber(1n), false);
+  assertStrictEquals(isNonNegativeNumber(-1n), false);
+
+  assertStrictEquals(isNonNegativeNumber(Number.NaN), false);
+  assertStrictEquals(isNonNegativeNumber(Number.POSITIVE_INFINITY), true);
+  assertStrictEquals(isNonNegativeNumber(Number.MAX_SAFE_INTEGER), true);
+  assertStrictEquals(isNonNegativeNumber(Number.MIN_SAFE_INTEGER), false);
+  assertStrictEquals(isNonNegativeNumber(Number.NEGATIVE_INFINITY), false);
+
+  assertStrictEquals(isNonNegativeNumber(undefined), false);
+  assertStrictEquals(isNonNegativeNumber(null), false);
+  assertStrictEquals(isNonNegativeNumber(true), false);
+  assertStrictEquals(isNonNegativeNumber(false), false);
+  assertStrictEquals(isNonNegativeNumber(""), false);
+  assertStrictEquals(isNonNegativeNumber("0"), false);
+});
+
+Deno.test("isNonPositiveNumber()", () => {
+  assertStrictEquals(isNonPositiveNumber(0), true);
+  assertStrictEquals(isNonPositiveNumber(-0), true);
+  assertStrictEquals(isNonPositiveNumber(1), false);
+  assertStrictEquals(isNonPositiveNumber(-1), true);
+
+  assertStrictEquals(isNonPositiveNumber(-10.1), true);
+  assertStrictEquals(isNonPositiveNumber(-9.9), true);
+  assertStrictEquals(isNonPositiveNumber(9.9), false);
+  assertStrictEquals(isNonPositiveNumber(10.1), false);
+
+  assertStrictEquals(isNonPositiveNumber(0n), false);
+  assertStrictEquals(isNonPositiveNumber(-0n), false);
+  assertStrictEquals(isNonPositiveNumber(1n), false);
+  assertStrictEquals(isNonPositiveNumber(-1n), false);
+
+  assertStrictEquals(isNonPositiveNumber(Number.NaN), false);
+  assertStrictEquals(isNonPositiveNumber(Number.POSITIVE_INFINITY), false);
+  assertStrictEquals(isNonPositiveNumber(Number.MAX_SAFE_INTEGER), false);
+  assertStrictEquals(isNonPositiveNumber(Number.MIN_SAFE_INTEGER), true);
+  assertStrictEquals(isNonPositiveNumber(Number.NEGATIVE_INFINITY), true);
+
+  assertStrictEquals(isNonPositiveNumber(undefined), false);
+  assertStrictEquals(isNonPositiveNumber(null), false);
+  assertStrictEquals(isNonPositiveNumber(true), false);
+  assertStrictEquals(isNonPositiveNumber(false), false);
+  assertStrictEquals(isNonPositiveNumber(""), false);
+  assertStrictEquals(isNonPositiveNumber("0"), false);
+});
+
+Deno.test("isNegativeNumber()", () => {
+  assertStrictEquals(isNegativeNumber(0), false);
+  assertStrictEquals(isNegativeNumber(-0), false);
+  assertStrictEquals(isNegativeNumber(1), false);
+  assertStrictEquals(isNegativeNumber(-1), true);
+
+  assertStrictEquals(isNegativeNumber(-10.1), true);
+  assertStrictEquals(isNegativeNumber(-9.9), true);
+  assertStrictEquals(isNegativeNumber(9.9), false);
+  assertStrictEquals(isNegativeNumber(10.1), false);
+
+  assertStrictEquals(isNegativeNumber(0n), false);
+  assertStrictEquals(isNegativeNumber(-0n), false);
+  assertStrictEquals(isNegativeNumber(1n), false);
+  assertStrictEquals(isNegativeNumber(-1n), false);
+
+  assertStrictEquals(isNegativeNumber(Number.NaN), false);
+  assertStrictEquals(isNegativeNumber(Number.POSITIVE_INFINITY), false);
+  assertStrictEquals(isNegativeNumber(Number.MAX_SAFE_INTEGER), false);
+  assertStrictEquals(isNegativeNumber(Number.MIN_SAFE_INTEGER), true);
+  assertStrictEquals(isNegativeNumber(Number.NEGATIVE_INFINITY), true);
+
+  assertStrictEquals(isNegativeNumber(undefined), false);
+  assertStrictEquals(isNegativeNumber(null), false);
+  assertStrictEquals(isNegativeNumber(true), false);
+  assertStrictEquals(isNegativeNumber(false), false);
+  assertStrictEquals(isNegativeNumber(""), false);
+  assertStrictEquals(isNegativeNumber("0"), false);
 });
 
 Deno.test("isSafeInteger()", () => {
