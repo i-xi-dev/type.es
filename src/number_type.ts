d20620872e2bad7d8c974ad741146d3fe4a7ea52
i-xi-dev <naoyuki.isono@gmail.com>
2024-11-01 01:23:48 +0900

    2.5.0

--- src/number_type.ts
+++ src/number_type.ts
@@ -56,8 +56,15 @@ export function isInRange<T extends number>(
   min: T,
   max: T,
 ): test is T {
-  //TODO isNumber(min)
-  //TODO isNumber(max)
+  assertNumber(min, "min");
+  assertNumber(max, "max");
+  if (Number.isNaN(min)) {
+    throw new TypeError("`min` must not be `NaN`.");
+  }
+  if (Number.isNaN(max)) {
+    throw new TypeError("`max` must not be `NaN`.");
+  }
+
   return isNumber(test) && (min <= test) && (max >= test);
 }
 
--- tests/number_type.test.ts
+++ tests/number_type.test.ts
@@ -442,13 +442,13 @@ Deno.test("NumberType.isInRange()", () => {
     NumberType.isInRange(0, 0, Number.POSITIVE_INFINITY),
     true,
   );
-  assertStrictEquals(NumberType.isInRange(0, 0, Number.NaN), false);
+  // assertStrictEquals(NumberType.isInRange(0, 0, Number.NaN), false);
   assertStrictEquals(NumberType.isInRange(0, Number.MIN_SAFE_INTEGER, 0), true);
   assertStrictEquals(
     NumberType.isInRange(0, Number.NEGATIVE_INFINITY, 0),
     true,
   );
-  assertStrictEquals(NumberType.isInRange(0, Number.NaN, 0), false);
+  // assertStrictEquals(NumberType.isInRange(0, Number.NaN, 0), false);
 
   assertStrictEquals(
     NumberType.isInRange(0, 1, Number.MAX_SAFE_INTEGER),
@@ -458,13 +458,13 @@ Deno.test("NumberType.isInRange()", () => {
     NumberType.isInRange(0, 1, Number.POSITIVE_INFINITY),
     false,
   );
-  assertStrictEquals(NumberType.isInRange(0, 1, Number.NaN), false);
+  // assertStrictEquals(NumberType.isInRange(0, 1, Number.NaN), false);
   assertStrictEquals(NumberType.isInRange(0, Number.MIN_SAFE_INTEGER, 1), true);
   assertStrictEquals(
     NumberType.isInRange(0, Number.NEGATIVE_INFINITY, 1),
     true,
   );
-  assertStrictEquals(NumberType.isInRange(0, Number.NaN, 1), false);
+  // assertStrictEquals(NumberType.isInRange(0, Number.NaN, 1), false);
 
   assertStrictEquals(
     NumberType.isInRange(0, -1, Number.MAX_SAFE_INTEGER),
@@ -474,7 +474,7 @@ Deno.test("NumberType.isInRange()", () => {
     NumberType.isInRange(0, -1, Number.POSITIVE_INFINITY),
     true,
   );
-  assertStrictEquals(NumberType.isInRange(0, -1, Number.NaN), false);
+  // assertStrictEquals(NumberType.isInRange(0, -1, Number.NaN), false);
   assertStrictEquals(
     NumberType.isInRange(0, Number.MIN_SAFE_INTEGER, -1),
     false,
@@ -483,7 +483,43 @@ Deno.test("NumberType.isInRange()", () => {
     NumberType.isInRange(0, Number.NEGATIVE_INFINITY, -1),
     false,
   );
-  assertStrictEquals(NumberType.isInRange(0, Number.NaN, -1), false);
+  // assertStrictEquals(NumberType.isInRange(0, Number.NaN, -1), false);
+
+  const e1 = "`min` must be a `number`.";
+  assertThrows(
+    () => {
+      NumberType.isInRange(0, undefined as unknown as number, 0);
+    },
+    TypeError,
+    e1,
+  );
+
+  const e2 = "`max` must be a `number`.";
+  assertThrows(
+    () => {
+      NumberType.isInRange(0, 0, undefined as unknown as number);
+    },
+    TypeError,
+    e2,
+  );
+
+  const e3 = "`min` must not be `NaN`.";
+  assertThrows(
+    () => {
+      NumberType.isInRange(0, Number.NaN, 0);
+    },
+    TypeError,
+    e3,
+  );
+
+  const e4 = "`max` must not be `NaN`.";
+  assertThrows(
+    () => {
+      NumberType.isInRange(0, 0, Number.NaN);
+    },
+    TypeError,
+    e4,
+  );
 });
 
 Deno.test("NumberType.normalize()", () => {
