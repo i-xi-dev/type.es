09f7444f1cd061ad4b8cb3b9f1ea9fde9d64c59e
i-xi-dev <naoyuki.isono@gmail.com>
2024-11-13 22:13:26 +0900

    4.0.0

--- src/_2/array_buffer_type.ts
+++ src/_2/array_buffer_type.ts
@@ -1,13 +1,44 @@
-import { assertIterable as assertIterableObject } from "../_0/object_type.ts";
+import {
+  assertAsyncIterable as assertAsyncIterableObject,
+  assertIterable as assertIterableObject,
+} from "../_0/object_type.ts";
+import { GrowableBuffer } from "./growable_buffer.ts";
+import { int, uint8 } from "../_.ts";
 import { Uint8 } from "./uint_type.ts";
 
+// const _DEFAULT_BYTE_LENGTH = 1_024;
+// const _DEFAULT_MAX_BYTE_LENGTH = 1_048_576;
+
+//TODO
+// export type FromUint8IterableOptions = {
+//   maxByteLength: int;
+// };
+
 export function fromUint8Iterable(
-  value: Iterable<number /* Uint8 */>,
+  value: Iterable<int /* uint8 */>,
+  // options?: FromUint8IterableOptions,
 ): ArrayBuffer {
   assertIterableObject(value, "value");
 
-  return Uint8Array.from(value, (i, index) => {
-    Uint8.assert(i, `value[${index}]`);
-    return i;
+  return Uint8Array.from(value, (byte, index) => {
+    Uint8.assert(byte, `value[${index}]`);
+    return byte;
   }).buffer;
+}
+
+export async function fromUint8AsyncIterable(
+  value: AsyncIterable<int /* uint8 */>,
+  // options?: FromUint8IterableOptions,
+): Promise<ArrayBuffer> {
+  assertAsyncIterableObject(value, "value");
+
+  //TODO resizable ArrayBufferが広く実装されたらそちらに変更する
+  const gb = new GrowableBuffer();
+  let index = 0;
+  for await (const byte of value) {
+    Uint8.assert(byte, `value[${index}]`);
+    gb.put(byte as uint8);
+    index++;
+  }
+  return gb.slice().buffer;
 }
