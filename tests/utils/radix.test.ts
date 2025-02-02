e36f6e3d462de7d1ee1f65fa1ed7ca1b446d5965
i-xi-dev <naoyuki.isono@gmail.com>
2025-02-02 10:40:42 +0900

    6.0.0

---
+++ tests/utils/radix.test.ts
@@ -0,0 +1,70 @@
+import {
+  assertStrictEquals,
+  assertThrows,
+  fail,
+  unreachable,
+} from "@std/assert";
+import { Radix } from "../../mod.ts";
+
+Deno.test("Radix.assertSupportedRadix()", () => {
+  try {
+    Radix.assertSupportedRadix(2, "test");
+    Radix.assertSupportedRadix(8, "test");
+    Radix.assertSupportedRadix(10, "test");
+    Radix.assertSupportedRadix(16, "test");
+  } catch (exception) {
+    fail((exception as Error).toString());
+  }
+
+  try {
+    Radix.assertSupportedRadix(0, "test");
+    unreachable();
+  } catch {
+    //
+  }
+  try {
+    Radix.assertSupportedRadix(1, "test");
+    unreachable();
+  } catch {
+    //
+  }
+  try {
+    Radix.assertSupportedRadix(3, "test");
+    unreachable();
+  } catch {
+    //
+  }
+});
+
+Deno.test("Radix.integerPatternOf()", () => {
+  assertStrictEquals(Radix.integerPatternOf(2), "^[01]+$");
+  assertStrictEquals(Radix.integerPatternOf(8), "^[0-7]+$");
+  assertStrictEquals(Radix.integerPatternOf(10), "^[0-9]+$");
+  assertStrictEquals(Radix.integerPatternOf(16), "^[0-9a-fA-F]+$");
+
+  const op = { includesSign: true } as const;
+  assertStrictEquals(Radix.integerPatternOf(2, op), "^[-+]?[01]+$");
+
+  assertThrows(
+    () => {
+      Radix.integerPatternOf(3 as 2);
+    },
+    TypeError,
+    "`radix` must be 2, 8, 10 or 16.",
+  );
+});
+
+Deno.test("Radix.prefixOf()", () => {
+  assertStrictEquals(Radix.prefixOf(2), "0b");
+  assertStrictEquals(Radix.prefixOf(8), "0o");
+  assertStrictEquals(Radix.prefixOf(10), "");
+  assertStrictEquals(Radix.prefixOf(16), "0x");
+
+  assertThrows(
+    () => {
+      Radix.prefixOf(3 as 2);
+    },
+    TypeError,
+    "`radix` must be 2, 8, 10 or 16.",
+  );
+});
