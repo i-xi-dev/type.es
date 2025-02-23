aacd99b7e8b6c3a3c4998f98f695ce67fe7a1275
i-xi-dev <naoyuki.isono@gmail.com>
2025-02-23 23:35:49 +0900

    6.0.0

--- src/__numerics/mod.ts
+++ src/__numerics/mod.ts
@@ -1,3 +1,2 @@
 export * from "./main.ts";
-export * from "./overflow_mode.ts";
 export * as Uint8Iterable from "./uint8_iterable.ts";
--- src/__numerics/overflow_mode.ts
+++
@@ -1,7 +0,0 @@
-export const OverflowMode = {
-  EXCEPTION: "exception",
-  TRUNCATE: "truncate",
-  SATURATE: "saturate",
-} as const;
-
-export type OverflowMode = typeof OverflowMode[keyof typeof OverflowMode];
