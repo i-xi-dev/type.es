e2cdd761579e6ad1fcdb50cf7881419cde32a48a
i-xi-dev <naoyuki.isono@gmail.com>
2025-03-12 23:26:27 +0900

    7.0.0

---
+++ src/text/encoding/utf_32/decoder.ts
@@ -0,0 +1,50 @@
+import {
+  _BE_LABEL,
+  _decodeBe,
+  _decodeLe,
+  _DEFAULT_REPLACEMENT_CHAR,
+  _getReplacement,
+  _LE_LABEL,
+  _MAX_BYTES_PER_RUNE,
+} from "./_common.ts";
+import { Decoder } from "../main.ts";
+
+export type Utf32DecoderOptions = {
+  fatal?: boolean;
+  ignoreBOM?: boolean;
+  // strict?: boolean;
+};
+
+/** @deprecated */
+export class Utf32BeDecoder extends Decoder {
+  constructor(options: Utf32DecoderOptions = {}) {
+    super({
+      name: _BE_LABEL,
+      fatal: options?.fatal === true,
+      replacementRune: _getReplacement(_DEFAULT_REPLACEMENT_CHAR, false).rune,
+      decode: _decodeBe,
+      ignoreBOM: options?.ignoreBOM === true,
+      // strict: options?.strict === true,
+      maxBytesPerRune: _MAX_BYTES_PER_RUNE,
+    });
+  }
+}
+
+//XXX Utf32BeDecoderStream
+
+/** @deprecated */
+export class Utf32LeDecoder extends Decoder {
+  constructor(options: Utf32DecoderOptions = {}) {
+    super({
+      name: _LE_LABEL,
+      fatal: options?.fatal === true,
+      replacementRune: _getReplacement(_DEFAULT_REPLACEMENT_CHAR, true).rune,
+      decode: _decodeLe,
+      ignoreBOM: options?.ignoreBOM === true,
+      // strict: options?.strict === true,
+      maxBytesPerRune: _MAX_BYTES_PER_RUNE,
+    });
+  }
+}
+
+//XXX Utf32LeDecoderStream
