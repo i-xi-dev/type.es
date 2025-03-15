c7e44cd443114fcc1d428d18b0420333209eea20
i-xi-dev <naoyuki.isono@gmail.com>
2025-03-15 23:52:07 +0900

    7.0.0

---
+++ src/bytes/stream/reading_task.ts
@@ -0,0 +1,153 @@
+import * as Loading from "../../loading/mod.ts";
+import * as Type from "../../type/mod.ts";
+import { BytesBuilder } from "../../buffer/mod.ts";
+import { AbortError, InvalidStateError } from "../../basics/mod.ts";
+
+async function* _streamToAsyncGenerator<T>(
+  stream: ReadableStream<T>,
+): AsyncGenerator<T, void, void> {
+  const reader = stream.getReader();
+  try {
+    for (
+      let i = await reader.read();
+      (i.done !== true);
+      i = await reader.read()
+    ) {
+      yield i.value;
+    }
+  } catch (exception) {
+    throw exception;
+  } finally {
+    await reader.cancel(); // streamもcancelされる
+    reader.releaseLock();
+  }
+}
+
+type _Bytes = Uint8Array<ArrayBuffer>;
+
+/**
+ * The [`ReadableStream`](https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream) of `Uint8Array` or the async iterator of `Uint8Array`.
+ */
+export type ReadingSource =
+  | AsyncIterable<_Bytes>
+  | ReadableStream<_Bytes>
+  | Iterable<_Bytes>;
+//XXX ReadableStreamは、そのうちAsyncIterableになる → のこるはSafariのみ
+
+/**
+ * The byte stream reading task.
+ */
+export class ReadingTask extends Loading.Task<_Bytes> {
+  readonly #stream: AsyncIterable<_Bytes> | Iterable<_Bytes>;
+
+  /**
+   * @param stream - The byte stream.
+   * @param options - The reading options.
+   */
+  private constructor(stream: ReadingSource, options?: Loading.Options) {
+    super(options);
+    if (Type.isAsyncIterable(stream) || Type.isIterable(stream)) {
+      this.#stream = stream;
+    } // deno-lint-ignore no-explicit-any
+    else if ((stream as any) instanceof ReadableStream) {
+      // SafariではReadableStreamはAsyncIterableではないので
+      this.#stream = _streamToAsyncGenerator<_Bytes>(stream);
+    } else {
+      throw new TypeError(
+        "`stream` must be a `ReadableStream<Uint8Array>` or an `AsyncIterable<Uint8Array>`.",
+      );
+    }
+  }
+
+  static create(stream: ReadingSource, options?: Loading.Options): ReadingTask {
+    return new ReadingTask(stream, options);
+  }
+
+  /**
+   * @returns The `Promise` that fulfills with a read byte sequence.
+   */
+  override async run(): Promise<_Bytes> {
+    const result = await this.#readAsyncIterable(
+      this.#stream as AsyncIterable<_Bytes>,
+    );
+    return result;
+  }
+
+  #throwIfAborted(signal?: AbortSignal): void {
+    if (signal?.aborted === true) {
+      const reason = signal.reason;
+      if (
+        (reason instanceof Error) &&
+        ((reason.name === "AbortError") || (reason.name === "TimeoutError"))
+      ) {
+        throw reason;
+      } else {
+        const abortError = new AbortError("Aborted via `options.signal`.");
+        abortError.cause = reason;
+        throw abortError;
+      }
+    }
+  }
+
+  async #readAsyncIterable(
+    asyncSource: AsyncIterable<_Bytes>,
+  ): Promise<_Bytes> {
+    if (this.status !== Loading.Status.READY) {
+      throw new InvalidStateError("`status` is not READY");
+    }
+    this._status = Loading.Status.RUNNING;
+
+    const builder = new BytesBuilder(
+      (this.indeterminate === true) ? undefined : { capacity: this.total },
+    );
+
+    try {
+      // started
+      this._notifyProgress("loadstart");
+
+      for await (const chunk of asyncSource) {
+        // this._signal?.throwIfAborted(); // reasonありでabort()した場合、 reasonをそのままスローするのでabortだったのかわからなくなる
+        this.#throwIfAborted(this._signal);
+
+        Type.assertUint8Array(chunk, "chunk");
+
+        builder.append(chunk);
+        this._loaded = builder.length;
+        this._notifyProgress("progress");
+      }
+
+      // completed
+      this._status = Loading.Status.COMPLETED;
+      // this._notifyProgress("load"); resolveされるのでわかる
+
+      return builder.toUint8Array();
+    } catch (exception) {
+      if ((exception instanceof Error) && (exception.name === "AbortError")) {
+        // ・呼び出し側のAbortControllerでreason省略でabortした場合
+        // ・呼び出し側のAbortControllerでreason:AbortErrorでabortした場合
+        this._status = Loading.Status.ABORTED;
+        // this._notifyProgress("abort"); rejectされるのでわかる
+      } else if (
+        (exception instanceof Error) && (exception.name === "TimeoutError")
+      ) {
+        // ・AbortSignal.timeoutでabortされた場合
+        // ・呼び出し側のAbortControllerでreason:TimeoutErrorでabortした場合
+        this._status = Loading.Status.ABORTED; //TODO timeout独自のstateにする？
+        // this._notifyProgress("timeout"); rejectされるのでわかる
+      } else {
+        // ・呼び出し側のAbortControllerでreason:AbortError,TimeoutError以外でabortした場合
+        // ・その他のエラー
+        this._status = Loading.Status.ERROR;
+        // this._notifyProgress("error"); rejectされるのでわかる
+      }
+      throw exception;
+    } finally {
+      // // signalに追加したリスナーを削除
+      // this.#abortController.abort();
+
+      this._notifyProgress("loadend"); // "progress"は間引く可能性があるので、最終的にloadedがいくつなのかは"progress"ではわからない
+    }
+  }
+}
+
+//TODO エラーメッセージ
---
+++ tests/bytes/stream/main.test.ts
@@ -0,0 +1,436 @@
+import { assertStrictEquals } from "@std/assert";
+import { Bytes } from "../../../mod.ts";
+
+function createStream(length: number) {
+  // deno-lint-ignore no-explicit-any
+  let ti: any;
+  return new ReadableStream({
+    start(controller: ReadableStreamDefaultController) {
+      let c = 0;
+      ti = setInterval(() => {
+        if (c >= length) {
+          clearInterval(ti);
+          controller.close();
+          return;
+        }
+        c = c + 1;
+
+        controller.enqueue(Uint8Array.of(255));
+      }, 10);
+    },
+    cancel() {
+      clearInterval(ti);
+    },
+  });
+}
+
+Deno.test("Bytes.ReadingTask.create() - ReadableStream", async () => {
+  const s1 = createStream(8);
+
+  const task1 = Bytes.ReadingTask.create(s1);
+  const bs1 = await task1.run();
+
+  assertStrictEquals(bs1.byteLength, 8);
+
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "InvalidStateError");
+    assertStrictEquals(err.message, "`status` is not READY");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - AsyncIterable", async () => {
+  const ai1 = async function* () {
+    yield Uint8Array.of(1);
+    yield Uint8Array.of(2, 3);
+  };
+  const task1 = Bytes.ReadingTask.create(ai1());
+  const bs1 = await task1.run();
+  assertStrictEquals(bs1.byteLength, 3);
+
+  // deno-lint-ignore require-yield
+  const ai2 = async function* (): AsyncGenerator<
+    Uint8Array<ArrayBuffer>,
+    void,
+    void
+  > {
+    return;
+  };
+  const reader2 = Bytes.ReadingTask.create(ai2());
+  const bs2 = await reader2.run();
+  assertStrictEquals(bs2.byteLength, 0);
+});
+
+Deno.test("Bytes.ReadingTask.create() - argumenterror", async () => {
+  try {
+    const task1 = Bytes.ReadingTask.create(
+      3 as unknown as AsyncIterable<Uint8Array<ArrayBuffer>>,
+    );
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "TypeError");
+    assertStrictEquals(
+      err.message,
+      "`stream` must be a `ReadableStream<Uint8Array>` or an `AsyncIterable<Uint8Array>`.",
+    );
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - Iterable", async () => {
+  const task1 = Bytes.ReadingTask.create([
+    Uint8Array.of(1),
+    Uint8Array.of(2, 3),
+  ]);
+  const bs1 = await task1.run();
+  assertStrictEquals(bs1.byteLength, 3);
+
+  const reader2 = Bytes.ReadingTask.create([] as Uint8Array<ArrayBuffer>[]);
+  const bs2 = await reader2.run();
+  assertStrictEquals(bs2.byteLength, 0);
+});
+
+Deno.test("Bytes.ReadingTask.prototype.run() - typeerror of chunk", async () => {
+  const ai1 = async function* () {
+    yield 1;
+    yield 2;
+  };
+  const task1 = Bytes.ReadingTask.create(
+    ai1() as unknown as AsyncIterable<Uint8Array<ArrayBuffer>>,
+  );
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "TypeError");
+    assertStrictEquals(
+      err.message,
+      "`chunk` must be an `Uint8Array<ArrayBuffer>`.",
+    );
+  }
+});
+
+Deno.test("Bytes.ReadingTask.prototype.run() - typeerror of chunk", async () => {
+  try {
+    const task1 = Bytes.ReadingTask.create(
+      [3] as unknown as Iterable<Uint8Array<ArrayBuffer>>,
+    );
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "TypeError");
+    assertStrictEquals(
+      err.message,
+      "`chunk` must be an `Uint8Array<ArrayBuffer>`.",
+    );
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - total", async () => {
+  const s1 = createStream(8);
+  const task1 = Bytes.ReadingTask.create(s1, { total: 8 });
+  const bs1 = await task1.run();
+  assertStrictEquals(bs1.byteLength, 8);
+  assertStrictEquals(bs1.buffer.byteLength, 8);
+
+  const s2 = createStream(8);
+  const reader2 = Bytes.ReadingTask.create(s2, { total: 9 });
+  const bs2 = await reader2.run();
+  assertStrictEquals(bs2.byteLength, 8);
+  assertStrictEquals(bs2.buffer.byteLength, 8);
+
+  const s3 = createStream(8);
+  const reader3 = Bytes.ReadingTask.create(s3, { total: 7 });
+  const bs3 = await reader3.run();
+  assertStrictEquals(bs3.byteLength, 8);
+  assertStrictEquals(bs3.buffer.byteLength, 8);
+
+  const s4 = createStream(8);
+  const reader4 = Bytes.ReadingTask.create(s4, { total: 0 });
+  const bs4 = await reader4.run();
+  assertStrictEquals(bs4.byteLength, 8);
+  assertStrictEquals(bs4.buffer.byteLength, 8);
+
+  const s5 = createStream(8);
+  const reader5 = Bytes.ReadingTask.create(s5, {
+    total: undefined,
+  });
+  const bs5 = await reader5.run();
+  assertStrictEquals(bs5.byteLength, 8);
+  assertStrictEquals(bs5.buffer.byteLength, 8);
+});
+
+Deno.test("Bytes.ReadingTask.create() - signal:AbortController.abort()", async () => {
+  const s1 = createStream(100);
+  const ac1 = new AbortController();
+  const task1 = Bytes.ReadingTask.create(s1, { signal: ac1.signal });
+  try {
+    setTimeout(() => {
+      ac1.abort();
+    }, 5);
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "AbortError");
+    console.log(err.message);
+    assertStrictEquals(task1.status, "aborted");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - signal:already aborted", async () => {
+  const s1 = createStream(100);
+  const task1 = Bytes.ReadingTask.create(s1, {
+    signal: AbortSignal.abort(new RangeError("1122")),
+  });
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "AbortError");
+    assertStrictEquals((err.cause as Error).name, "RangeError");
+    console.log(err.message);
+    assertStrictEquals(task1.status, "aborted");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - signal:already aborted", async () => {
+  const s2 = createStream(100);
+  const ac2 = new AbortController();
+  ac2.abort(new RangeError("123"));
+  const task1 = Bytes.ReadingTask.create(s2, { signal: ac2.signal });
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "AbortError");
+    assertStrictEquals((err.cause as Error).name, "RangeError");
+    assertStrictEquals(task1.status, "aborted");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - signal:already aborted", async () => {
+  const s2 = createStream(100);
+  const ac2 = new AbortController();
+  ac2.abort();
+  const task1 = Bytes.ReadingTask.create(s2, { signal: ac2.signal });
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "AbortError");
+    assertStrictEquals(task1.status, "aborted");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.create() - signal:AbortSignal.timeout", async () => {
+  const s1 = createStream(100);
+  const task1 = Bytes.ReadingTask.create(s1, {
+    signal: AbortSignal.timeout(5),
+  });
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "TimeoutError");
+    console.log(err.message);
+    assertStrictEquals(task1.status, "aborted");
+  }
+});
+
+Deno.test("Bytes.ReadingTask.prototype.addEventListener()", async () => {
+  const s1 = createStream(8);
+  const task1 = Bytes.ReadingTask.create(s1);
+
+  let loadedLength = -1;
+  let totalLength = -1;
+  let lengthComputable = undefined;
+  const names: string[] = [];
+  const listener = (e: ProgressEvent) => {
+    names.push(e.type);
+    loadedLength = e.loaded;
+    totalLength = e.total;
+    lengthComputable = e.lengthComputable;
+  };
+
+  task1.addEventListener("loadstart", listener as EventListener);
+  // task1.addEventListener("load", listener as EventListener);
+  task1.addEventListener("progress", listener as EventListener);
+  // task1.addEventListener("abort", listener as EventListener);
+  // task1.addEventListener("timeout", listener as EventListener);
+  // task1.addEventListener("error", listener as EventListener);
+  task1.addEventListener("loadend", listener as EventListener);
+
+  const bs1 = await task1.run();
+  assertStrictEquals(bs1.byteLength, 8);
+  assertStrictEquals(loadedLength, 8);
+  assertStrictEquals(totalLength, 0);
+  assertStrictEquals(lengthComputable, false);
+  assertStrictEquals(names.filter((n) => n === "loadstart").length, 1);
+  // assertStrictEquals(names.filter((n) => n === "load").length, 1);
+  assertStrictEquals(names.filter((n) => n === "progress").length >= 1, true);
+  // assertStrictEquals(names.filter((n) => n === "abort").length, 0);
+  // assertStrictEquals(names.filter((n) => n === "timeout").length, 0);
+  // assertStrictEquals(names.filter((n) => n === "error").length, 0);
+  assertStrictEquals(names.filter((n) => n === "loadend").length, 1);
+});
+
+Deno.test("Bytes.ReadingTask.prototype.addEventListener() - total", async () => {
+  const s1 = createStream(8);
+  const task1 = Bytes.ReadingTask.create(s1, { total: 200 });
+
+  let loadedLength = -1;
+  let totalLength = -1;
+  let lengthComputable = undefined;
+  const names: string[] = [];
+  const listener = (e: ProgressEvent) => {
+    names.push(e.type);
+    loadedLength = e.loaded;
+    totalLength = e.total;
+    lengthComputable = e.lengthComputable;
+  };
+
+  task1.addEventListener("loadstart", listener as EventListener);
+  // task1.addEventListener("load", listener as EventListener);
+  task1.addEventListener("progress", listener as EventListener);
+  // task1.addEventListener("abort", listener as EventListener);
+  // task1.addEventListener("timeout", listener as EventListener);
+  // task1.addEventListener("error", listener as EventListener);
+  task1.addEventListener("loadend", listener as EventListener);
+
+  const bs1 = await task1.run();
+  assertStrictEquals(bs1.byteLength, 8);
+  assertStrictEquals(loadedLength, 8);
+  assertStrictEquals(totalLength, 200);
+  assertStrictEquals(lengthComputable, true);
+  assertStrictEquals(names.filter((n) => n === "loadstart").length, 1);
+  // assertStrictEquals(names.filter((n) => n === "load").length, 1);
+  assertStrictEquals(names.filter((n) => n === "progress").length >= 1, true);
+  // assertStrictEquals(names.filter((n) => n === "abort").length, 0);
+  // assertStrictEquals(names.filter((n) => n === "timeout").length, 0);
+  // assertStrictEquals(names.filter((n) => n === "error").length, 0);
+  assertStrictEquals(names.filter((n) => n === "loadend").length, 1);
+});
+
+Deno.test("Bytes.ReadingTask.prototype.addEventListener() - abort", async () => {
+  const s1 = createStream(8);
+  const ac1 = new AbortController();
+  const task1 = Bytes.ReadingTask.create(s1, { signal: ac1.signal });
+  let loadedLength = -1;
+  let totalLength = -1;
+  let lengthComputable = undefined;
+  const names: string[] = [];
+  const listener = (e: ProgressEvent) => {
+    names.push(e.type);
+    loadedLength = e.loaded;
+    totalLength = e.total;
+    lengthComputable = e.lengthComputable;
+  };
+
+  task1.addEventListener("loadstart", listener as EventListener);
+  // task1.addEventListener("load", listener as EventListener);
+  task1.addEventListener("progress", listener as EventListener);
+  // task1.addEventListener("abort", listener as EventListener);
+  // task1.addEventListener("timeout", listener as EventListener);
+  // task1.addEventListener("error", listener as EventListener);
+  task1.addEventListener("loadend", listener as EventListener);
+
+  setTimeout(() => {
+    ac1.abort();
+  }, 20);
+  try {
+    await task1.run();
+    throw new Error();
+  } catch (ex) {
+    void ex;
+  }
+  //assertStrictEquals(bs1.byteLength, 8);
+  assertStrictEquals(loadedLength >= 1, true);
+  assertStrictEquals(totalLength, 0);
+  assertStrictEquals(lengthComputable, false);
+  assertStrictEquals(names.filter((n) => n === "loadstart").length, 1);
+  // assertStrictEquals(names.filter((n) => n === "load").length, 0);
+  assertStrictEquals(names.filter((n) => n === "progress").length >= 1, true);
+  // assertStrictEquals(names.filter((n) => n === "abort").length, 1);
+  // assertStrictEquals(names.filter((n) => n === "timeout").length, 0);
+  // assertStrictEquals(names.filter((n) => n === "error").length, 0);
+  assertStrictEquals(names.filter((n) => n === "loadend").length, 1);
+});
+
+//TODO
+// Bytes.ReadingTask.prototype.addEventListener() - error
+// Bytes.ReadingTask.prototype.status - error
+
+Deno.test("Bytes.ReadingTask.prototype.status", async () => {
+  const s1 = createStream(80);
+  const task1 = Bytes.ReadingTask.create(s1);
+
+  assertStrictEquals(task1.status, "ready");
+
+  const tasks = [task1.run()];
+  assertStrictEquals(task1.status, "running");
+  const bs1 = await Promise.all(tasks);
+  assertStrictEquals(bs1[0].byteLength, 80);
+  assertStrictEquals(task1.status, "completed");
+});
+
+Deno.test("Bytes.ReadingTask.prototype.loaded/total/indetrminate", async () => {
+  const s1 = createStream(80);
+  const task1 = Bytes.ReadingTask.create(s1);
+
+  assertStrictEquals(task1.total, 0);
+  assertStrictEquals(task1.loaded, 0);
+  assertStrictEquals(task1.indeterminate, true);
+
+  const tasks = [task1.run()];
+  const bs1 = await Promise.all(tasks);
+  assertStrictEquals(bs1[0].byteLength, 80);
+
+  assertStrictEquals(task1.total, 0);
+  assertStrictEquals(task1.loaded, 80);
+  assertStrictEquals(task1.indeterminate, true);
+});
+
+Deno.test("Bytes.ReadingTask.prototype.loaded/total/indetrminate", async () => {
+  const s1 = createStream(80);
+  const task1 = Bytes.ReadingTask.create(s1, { total: 80 });
+
+  assertStrictEquals(task1.total, 80);
+  assertStrictEquals(task1.loaded, 0);
+  assertStrictEquals(task1.indeterminate, false);
+
+  const tasks = [task1.run()];
+  const bs1 = await Promise.all(tasks);
+  assertStrictEquals(bs1[0].byteLength, 80);
+
+  assertStrictEquals(task1.total, 80);
+  assertStrictEquals(task1.loaded, 80);
+  assertStrictEquals(task1.indeterminate, false);
+});
+
+Deno.test("Bytes.ReadingTask.prototype.loaded/total/indetrminate - abort", async () => {
+  const s1 = createStream(100);
+  const ac1 = new AbortController();
+  const task1 = Bytes.ReadingTask.create(s1, { signal: ac1.signal });
+  try {
+    setTimeout(() => {
+      ac1.abort();
+    }, 5);
+    await task1.run();
+    throw new Error();
+  } catch (e) {
+    const err = e as Error;
+    assertStrictEquals(err.name, "AbortError");
+    assertStrictEquals(task1.loaded >= 0, true);
+  }
+});
